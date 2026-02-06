2c82b4da0194e0916118844efcbe01d26dcc2f59
dtsteele17 <dtsteele17@gmail.com>
2026-02-06 01:42:11 +0000

    Updated supabase.ts

--- app/src/lib/supabase.ts
+++ app/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lpzdenlnqtxpleexgxyb.supabase.co';
+const supabaseUrl = '';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwemRlbmxucXR4cGxlZXhneHliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyOTAyNTcsImV4cCI6MjA4NTg2NjI1N30.9lqOUFtuENc8bPESZxK_gSX4tp_4ijlpEL6YsWKm3v4';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
